7d25c8c40fdaf61617875904d4846ebb5d232b50
Vamangi <75316070+Vamangi@users.noreply.github.com>
2021-05-02 18:52:37 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 100,300)
+    fill("lightblue")
+    text("Hint: Stores all Information !!", 100,320)
+    
+    
+    fill("white")
+    textSize(15)
+    text("", 600,80)
+    text("C U T N I F O N", 600,80);
+    fill("lightblue")
+    text("Hint: Performs a particular Task !!", 600,100)
+
+}
